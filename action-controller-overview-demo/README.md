00a6e73ee543c8d153926d553cbeb995f63a474a
JiangYongKang <vincent4502237@gmail.com>
2017-10-17 11:42:22 +0800

    action controller overview demo.

--- action-controller-overview-demo/README.md
+++ action-controller-overview-demo/README.md
@@ -1,1 +1,20 @@
+```zsh
+$ tree app/controllers app/models app/views
+app/controllers
+├── application_controller.rb
+├── clients_controller.rb
+└── concerns
+app/models
+├── application_record.rb
+├── client.rb
+└── concerns
+app/views
+├── clients
+│   └── index.json.jbuilder
+└── layouts
+    ├── application.html.erb
+    ├── mailer.html.erb
+    └── mailer.text.erb
 
+4 directories, 8 files
+```
--- action-controller-overview-demo/app/controllers/clients_controller.rb
+++ action-controller-overview-demo/app/controllers/clients_controller.rb
@@ -1,9 +1,19 @@
 # 控制器命名约定，最后一个单词采用复数形式，控制器必须继承自 ApplicationController
 class ClientsController < ApplicationController
 
-  # 访问 /clients.json 或者 /clients 会默认渲染 new.json.jbuilder 或者 new.html.erb
+  # 访问 /clients/new.json 或者 /clients/new 会默认渲染 new.json.jbuilder 或者 new.html.erb
   # 可以在视图中使用 @client 实例变量
   def new
     @client = Client.new
+  end
+
+  # /clients?status=1
+  # /clients?status=true
+  # /clients?status=ok
+  # /clients?status[]=open&status[]=close
+  # 请求参数的类型不会自动转换，统一的被处理为字符串或数组
+  def index
+    logger.info "  [LOGGER INFO] params[:status].class = #{params[:status].class}"
+    @parameters = params
   end
 end
